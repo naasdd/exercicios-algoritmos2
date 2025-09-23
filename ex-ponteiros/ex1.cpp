8a4fa8dfe1f0da17478a2415b6315d7f165cfc9a
naasdd <josegabrielg2014@gmail.com>
2025-09-23 01:24:59 -0300

    rascunho de testes sobre ponteiro

---
+++ ex-ponteiros/ex1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+int main()
+{
+    struct DATA
+    {
+        string dia;
+    };
+
+    cout << sizeof(DATA) << endl;
+
+    return 0;
+}
