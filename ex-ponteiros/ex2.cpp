fe527e7362552a164536684fda61618d5a33bbbd
naasdd <josegabrielg2014@gmail.com>
2025-09-24 23:43:00 -0300

    jaldkjdslak

--- ex-ponteiros/ex2.cpp
+++ ex-ponteiros/ex2.cpp
@@ -3,26 +3,12 @@
 using namespace std;
 
 // codigozinho besta só pra pegar a sintaxe e o básico de alocação dinâmica e vetores, nem é exercicio
+// objetivo é fazer um array que varia de tamanho dinâmicamente
 
 int main()
 {
-    int *p1;
-    int x = 5;
-    int teste[50];
-
-    p1 = &x;
-
-    x = 1;
-
-
-    // cout << "\np1 = " << p1 << endl;
-    // cout << "\nX = " << &x << endl;
-    cout << "\n&teste           = " << teste << endl;
-    cout << "\n&teste[0]        = " << &teste[0] << endl;
-    cout << "\n&teste[1]        = " << &teste[2] << endl;
-    cout << "\nsizeof teste[0]  = " << sizeof(teste[0]) << endl;
-
-
+    
+    
 
     return 0;
 }
