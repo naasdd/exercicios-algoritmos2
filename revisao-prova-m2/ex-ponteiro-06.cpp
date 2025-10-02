60bd41e2d3d082d3d2bf7e072ede1bbfaa60e119
naasdd <josegabrielg2014@gmail.com>
2025-10-02 18:51:27 -0300

    finaizado ex-ponteiro-06

--- revisao-prova-m2/ex-ponteiro-06.cpp
+++ revisao-prova-m2/ex-ponteiro-06.cpp
@@ -10,15 +10,54 @@ int main()
     int *p2;
     int v = 3;
 
-    cout << "p1 = " << *p1 << endl;
-    cout << "p2 = " << *p2 << endl;
+    cout << "\n\nTeste a)" << endl;
     cout << "v = " << v << endl;
 
-    cout << "\n\n";
 
     p2 = &v;
     p1 = new int;
 
+    cout << "\n\nTeste b)" << endl;
+    cout << "p1 = " << *p1 << endl;
+    cout << "p2 = " << *p2 << endl;
+    cout << "v = " << v << endl;
+
+
+    *p1 = 42;
+
+    cout << "\n\nTeste c)" << endl;
+    cout << "p1 = " << *p1 << endl;
+    cout << "p2 = " << *p2 << endl;
+    cout << "v = " << v << endl;
+
+
+    int *p3;
+
+    cout << "\n\nTeste d)" << endl;
+    cout << "p1 = " << *p1 << endl;
+    cout << "p2 = " << *p2 << endl;
+    cout << "v = " << v << endl;
+
+
+    p3 = &v;
+
+    cout << "\n\nTeste e)" << endl;
+    cout << "p1 = " << *p1 << endl;
+    cout << "p2 = " << *p2 << endl;
+    cout << "v = " << v << endl;
+
+
+    v = 88;
+
+    cout << "\n\nTeste f)" << endl;
+    cout << "p1 = " << *p1 << endl;
+    cout << "p2 = " << *p2 << endl;
+    cout << "v = " << v << endl;
+
+    
+    p1 = &v;
+
+    cout << "\n\nTeste g)" << endl;
     cout << "p1 = " << *p1 << endl;
     cout << "p2 = " << *p2 << endl;
     cout << "v = " << v << endl;
