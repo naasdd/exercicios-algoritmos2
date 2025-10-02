255fa6572acf8dbbe6d2c7b7f91a6d6d5206145d
naasdd <josegabrielg2014@gmail.com>
2025-10-02 16:30:04 -0300

    codando em porto alegre

--- revisao-prova-m2/ex-ponteiro-06.cpp
+++ revisao-prova-m2/ex-ponteiro-06.cpp
@@ -2,9 +2,26 @@
 
 using namespace std;
 
-int main(){
+int main()
+{
     cout << "\n-- Inicio do Programa --\n\n";
 
+    int *p1;
+    int *p2;
+    int v = 3;
+
+    cout << "p1 = " << *p1 << endl;
+    cout << "p2 = " << *p2 << endl;
+    cout << "v = " << v << endl;
+
+    cout << "\n\n";
+
+    p2 = &v;
+    p1 = new int;
+
+    cout << "p1 = " << *p1 << endl;
+    cout << "p2 = " << *p2 << endl;
+    cout << "v = " << v << endl;
 
     return 0;
 }
