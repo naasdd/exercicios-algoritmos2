8cc0b2b78957f9e0d6470cec23c5e109cc5d06b0
José Gabriel Santos Gomes <70996909+naasdd@users.noreply.github.com>
2025-10-02 05:07:06 -0300

    exercicio de recursividade finalizado

--- revisao-prova-m2/ex-recursividade-17.cpp
+++ revisao-prova-m2/ex-recursividade-17.cpp
@@ -1,21 +1,52 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
-int divisao(){
+std::vector<int> arrayBinario;
 
+int divisao(int userInput)
+{
 
+    int resto = (userInput % 2);
+    int dividido = (userInput / 2);
+
+    cout << userInput << " / 2 = " << dividido << ", resto " << resto << endl;
+
+    arrayBinario.push_back(resto);
+
+    if (dividido == 0)
+    {
+        return resto;
+    }
+    else
+    {
+        return divisao(dividido);
+    }
 }
 
-bool validarEntrada(int userInput){
-    if(userInput < 0 ){
+bool validarEntrada(int userInput)
+{
+    if (userInput < 0)
+    {
         return false;
     }
 
     return true;
 }
+void displayFinal(int userInput)
+{
+    cout << "\nResultado: " << userInput << " = ";
+    for (size_t i = 0; i < arrayBinario.size(); ++i)
+    {
+        cout << arrayBinario[i];
+    }
 
-int main(){
+    cout << endl;
+}
+
+int main()
+{
     system("chcp 65001");
     cout << "\n-- Transformar número inteiro em binário --\n\n";
 
@@ -23,10 +54,11 @@ int main(){
     cout << "Digite um número inteiro positivo: ";
     cin >> userInput;
 
-    if (validarEntrada(userInput)){
-        cout << "\nválido" << endl;
+    if (validarEntrada(userInput))
+    {
+        divisao(userInput);
+        displayFinal(userInput);
     }
-
 
     return 0;
 }
